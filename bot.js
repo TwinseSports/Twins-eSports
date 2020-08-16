179c935b820ecf79610c9d3e12bf55413d27c859
TwinseSports <68622019+TwinseSports@users.noreply.github.com>
2020-08-17 00:16:55 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,7 +23,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`SwixZ eSports`,"http://twitch.tv/SwixZ_eSports")
+client.user.setGame(`Coryell eSports`,"http://twitch.tv/Coryell_eSports")
 client.user.setStatus("dnd")
 });
 
